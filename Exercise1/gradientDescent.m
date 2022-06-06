1892278c19ab1ab8aa4dc505cec5ece8d492577e
Maria Hristova <fcmj@abv.bg>
2022-06-06 20:16:49 +0100

    Completed Exercise 1 (+ optional)

--- Exercise1/gradientDescent.m
+++ Exercise1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -9,23 +9,13 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
+    hypothesis = theta(1)*X(:,1) + theta(2)*X(:,2); % m x 1 vector
 
+    tSum = sum( (X .* (hypothesis - y) ) )'; % X is m x 2 matrix
 
+    theta = theta - ( alpha * (1/m) * tSum);
 
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
 
 end
--- Exercise1/gradientDescentMulti.m
+++ Exercise1/gradientDescentMulti.m
@@ -9,27 +9,13 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
+    hypothesis = X * theta; % m x 1 vector
 
+    tSum = sum( (X .* (hypothesis - y) ) )'; % X is m x 2 matrix
 
+    theta = theta - ( alpha * (1/m) * tSum);
 
-
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
