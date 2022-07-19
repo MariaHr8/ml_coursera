336ec8c64c0688091f3e7cbb534923521a275dd4
Maria Hristova <fcmj@abv.bg>
2022-07-19 13:32:24 +0100

    No printing of new_y

--- Exercise4/ex4.m
+++ Exercise4/ex4.m
@@ -113,7 +113,6 @@ fprintf('\nEvaluating sigmoid gradient...\n')
 
 g = sigmoidGradient([-1 -0.5 0 0.5 1]);
 fprintf('Sigmoid gradient evaluated at [-1 -0.5 0 0.5 1]:\n  ');
-fprintf('%f ', g);
 fprintf('\n\n');
 
 fprintf('Program paused. Press enter to continue.\n');
--- Exercise4/nnCostFunction.m
+++ Exercise4/nnCostFunction.m
@@ -9,10 +9,7 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
 %   nn_params and need to be converted back into the weight matrices. 
-% 
-%   The returned parameter grad should be a "unrolled" vector of the
-%   partial derivatives of the neural network.
-%
+
 
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
@@ -43,7 +40,7 @@ z3 = Theta2 * a2';
 hypo_theta = sigmoid(z3); % hypo_theta = a3
 
 % Recode labels as vectors containing only values 0 or 1
-new_y = zeros(num_labels, m) % 10*5000
+new_y = zeros(num_labels, m); % 10*5000
 for i = 1:m,                 % loop through every training example (i corresponding to index col)
     new_y(y(i), i) = 1;      % mark the corresponding y of the training example in the new y as 1
 end
@@ -65,15 +62,6 @@ J = J + Reg;
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
 %         Theta2_grad, respectively. After implementing Part 2, you can check
 %         that your implementation is correct by running checkNNGradients
-%
-%         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
-%               cost function.
-%
-%         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
-%               first time.
 
 % Back propagation
 for t=1:m
@@ -90,7 +78,7 @@ for t=1:m
 	z3 = Theta2 * a2; % (10*26)*(26*1)
 	a3 = sigmoid(z3); % final activation layer a3 == h(theta) (10*1)
     
-    % Step 3
+    % Step 3 - 𝛿
 	delta_3 = a3 - new_y(:,t); % (10*1)
     z2=[1; z2]; % bias (26*1)
 
@@ -105,22 +93,17 @@ for t=1:m
 end;
 
 % Step 5.2
-Theta2_grad = (1/m) * Theta2_grad; % (10*26)
-Theta1_grad = (1/m) * Theta1_grad; % (25*401)
-
-
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
+Theta2_grad = (1/m) * Theta2_grad; % (10*26) - used to set j = 0
+Theta1_grad = (1/m) * Theta1_grad; % (25*401) - used to set j = 0
 
 % Regularization
-
 Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + ((lambda/m) * Theta1(:, 2:end)); % for j >= 1 
 Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + ((lambda/m) * Theta2(:, 2:end)); % for j >= 1
 
+% 
+%   The returned parameter grad should be a "unrolled" vector of the
+%   partial derivatives of the neural network.
+%
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
